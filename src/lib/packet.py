14344241adbf2dad8792f816820d4f42b658086a
Emanuel Condo <emanuelcondo@Emanuels-MacBook-Air.local>
2025-04-25 02:51:26 -0300

    feat: add Packet class to handle messages (seq, ack, data)

---
+++ src/lib/packet.py
@@ -0,0 +1,30 @@
+from struct import pack, unpack
+
+
+class Packet:
+    def __init__(self, seq, ack, data=None):
+        self.seq = seq
+        self.ack = ack
+        self.data = data if data is not None else bytes()
+
+    @classmethod
+    def from_bytes(cls, bytes):
+        """
+        Parses bytes to generate a Packet instance
+        """
+
+        # 2 bytes for sequence number (unsigned short)
+        # 2 bytes for ack (unsigned short)
+        # len(bytes) - 4 bytes for data
+        # big-endian
+        seq, ack, data = unpack("!1H1H{}s".format(len(bytes) - 4), bytes)
+        return cls(seq, ack, data)
+
+    def to_bytes(self):
+        """
+        Converts the packet instance to bytes (seq, ack, data)
+        """
+
+        # big-endian
+        return pack("!1H1H{}s".format(len(self.data) - 4),
+                    self.seq, self.ack, self.data)

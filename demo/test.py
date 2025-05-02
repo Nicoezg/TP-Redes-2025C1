26adb8db203d81dd515db449e6a5ff9ac0eb1068
victoriafdelgado <vickyfdelgado@gmail.com>
2025-05-02 17:15:49 -0300

    feat: al crear las terminales xterm  se ejecutan los comandos y tienen un titulo

--- demo/test.py
+++ demo/test.py
@@ -1,6 +1,7 @@
 from mininet.net import Mininet
 from mininet.log import setLogLevel
-
+from mininet.term import makeTerm
+from time import sleep
 
 
 def run():
@@ -15,19 +16,31 @@ def run():
       
     h1, h2 = net.get('h1', 'h2')
     #TODO: Hacer que las xterm tengan nombres distintos (h1 y h2)
-    h1.cmd('xterm &')
-    h2.cmd('xterm &')
+
+    makeTerm(h2, title="server", cmd= 'python3 ../src/start-server.py -H 192.168.0.2 -p 8081 -s ../demo/srv -v; exec bash')
+    
+    while True:
+        operacion = input('What do you want to do? (upload/download): ')
+        if operacion == 'upload':
+            makeTerm(h1, title="client", cmd= 'python3 ../src/upload.py -H 192.168.0.2 -p 8081 -s media/4mb.pdf -n 4mb.pdf -r gbn -v; exec bash')
+        elif operacion == 'download':
+            makeTerm(h1, title="client", cmd= 'python3 ../src/download.py -H 192.168.0.2 -p 8081 -d ../demo/media/4mb.pdf -n 4mb.pdf -r gbn -v; exec bash')
+        
+        done = input("¿Terminaste? (y/n): ").strip().lower()
+        if done == 'y':
+            break
+
     # Upload:
     #(en h2) python3 ../src/start-server.py -H 192.168.0.2 -p 8081 -s ../demo/srv -v
     #(en h1) python3 ../src/upload.py -H 192.168.0.2 -p 8081 -s /media/4mb.pdf -n 4mb.pdf -r gbn -v
 
     # Download:
     # (en h1) python3 ../src/download.py -H 192.168.0.2 -p 8081 -d ../demo/clnt/4mb.pdf -n 4mb.pdf -r gbn -v
-    while input('Done? (y/n): ') != 'y':
-        pass
+   
     
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
     run()
+

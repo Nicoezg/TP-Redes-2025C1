dfc790f93e813b2c851cdfe9e53b58e194fc04db
ibertolini <56204692+nacho-1@users.noreply.github.com>
2025-06-18 19:11:57 -0300

    feat: Modificaciones al firewall
    Modifico la arquitectura del firewall.
    Todavia no esta terminado. Creo q falta que se instalen las reglas en
    los switches ya que no se hace automaticamente.

--- pox/ext/firewall.py
+++ pox/ext/firewall.py
@@ -16,22 +16,108 @@ from collections import namedtuple
 import os
 from pox.lib.addresses import IPAddr
 ''' Add your imports here ... '''
-
+# Constants for Ethernet header values
+from pox.lib.packet.ethernet import ethernet
+# Constants for IPv4 header values
+from pox.lib.packet.ipv4 import ipv4
 
 
 log = core.getLogger()
 policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]
 
 ''' Add your global variables here ... '''
+RULES_BY_DPID = {
+    1: [
+        {"tp_dst": 80, "action":"deny"},
+        {"nw_proto": 17, "nw_src": IPAddr("10.0.0.1"), "tp_dst": 5001, "action": "deny"},
+    ],
+}
 
 
 
-class Firewall (EventMixin):
+class Firewall (object):
+    """
+    A Firewall object is created for each switch that connects.
+    An Event object for that connection is passed to the __init__ function.
+    """
 
-    def __init__ (self):
-        self.listenTo(core.openflow)
-        log.debug("Enabling Firewall Module")
+    def __init__ (self, event):
+        log.debug("Starting firewall instance for connection %s (switch ID: %s)",\
+                event.connection,\
+                dpidToStr(event.dpid))
+        
+        # Keep track of connection to switch.
+        self.connection = event.connection
 
+        # Bind PacketIn event listener
+        # (and eventually others?).
+        event.connection.addListeners(self)
+    
+    def _handle_PacketIn(self, event):
+        in_port = event.port
+        dpid = event.connection.dpid # Unique ID for the switch.
+        packet = event.parsed
+
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
+        
+        # Extract basic fields
+        fields = {
+            "eth_type": packet.type,
+            "nw_proto": None, # Transport protocol
+            "nw_src": None, # IP src
+            "nw_dst": None, # IP dst
+            "tp_src": None, # src port
+            "tp_dst": None, # dst port
+        }
+
+        log.debug("Processing packet for connection %s (switch ID: %s)",\
+                self.connection,\
+                str(dpid))
+    
+        if fields["eth_type"] == ethernet.IP_TYPE:
+            ip_pkt = packet.payload
+            fields["nw_proto"] = ip_pkt.protocol
+            fields["nw_src"] = ip_pkt.srcip
+            fields["nw_dst"] = ip_pkt.dstip
+
+            if fields["nw_proto"] == ipv4.TCP_PROTOCOL\
+            or fields["nw_proto"] == ipv4.UDP_PROTOCOL:
+                transport_pkt = ip_pkt.payload
+                fields["tp_src"] = transport_pkt.srcport
+                fields["tp_dst"] = transport_pkt.dstport
+                if fields.get("tp_dst") == 80:
+                    log.debug("Destined to port 80")
+        
+        rules = RULES_BY_DPID.get(dpid)
+        if rules:
+            log.debug("%s rules for dpid %s", str(len(rules)), str(dpid))
+            for rule in rules:
+                if self.check_matches(rule, fields):
+                    if rule["action"] == "deny":
+                        log.debug("Dropped packet due to rule: %s", rule)
+                        return
+
+
+        # Default: allow/flood
+        log.debug("Packet allowed through firewall")
+        msg = of.ofp_packet_out()
+        msg.data = event.ofp
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        msg.in_port = in_port
+        self.connection.send(msg)
+
+    def check_matches(self, rule, fields):
+        for field, val in rule.items():
+            if field == "action": # Don't compare action
+                continue
+            if val == "any":
+                continue
+            if val != fields.get(field):
+                return False
+        return True
+    """
     def _handle_ConnectionUp (self, event):
 
         # Drop all TCP traffic to port 80
@@ -58,54 +144,11 @@ class Firewall (EventMixin):
         msg.match.tp_dst = 5001
         msg.actions = []
         event.connection.send(msg)
-
-"""     def _handle_PacketIn(self, event):
-        packet = event.parsed
-        if packet and packet.type == packet.IP_TYPE:
-            ip_packet = packet.payload
-
-            if ip_packet.protocol == ip_packet.TCP_PROTOCOL:
-                tcp_packet = ip_packet.payload
-                
-                if tcp_packet.dstport == 80:
-                    # Drop packet by not sending any actions
-                    msg = of.ofp_flow_mod()
-                    msg.actions = []           # No actions = drop
-                    event.connection.send(msg)
-                    return
-      
-            elif ip_packet.protocol == ip_packet.UDP_PROTOCOL:
-                udp_packet = ip_packet.payload
-
-                if udp_packet.dstport == 80:
-                    # Drop packet by not sending any actions
-                    msg = of.ofp_flow_mod()
-                    msg.actions = []  # No actions = drop
-                    event.connection.send(msg)
-                    return
-
-                if udp_packet.dstport == 5001 and ip_packet.srcip == EthAddr("00:00:00:00:00:01"):
-                    # Drop packet by not sending any actions
-                    msg = of.ofp_flow_mod()
-                    msg.actions = []
-                    event.connection.send(msg)
-                    return
-            
-            if (
-                    (ip_packet.srcip == EthAddr("00:00:00:00:00:01") and ip_packet.dstip == EthAddr("00:00:00:00:00:02")) or
-                    (ip_packet.srcip == EthAddr("00:00:00:00:00:02") and ip_packet.dstip == EthAddr("00:00:00:00:00:01"))
-                ):
-                    # Drop packet by not sending any actions
-                    msg = of.ofp_flow_mod()
-                    msg.match.dl_type = 0x800
-                    msg.actions = []
-                    return 
-        return """
-                
-        
-
+    """
+               
 def launch ():
     '''
     Starting the Firewall module
     '''
-    core.registerNew(Firewall)
+    # Automagically calls Firewall(event).
+    core.openflow.addListenerByName("ConnectionUp", Firewall)

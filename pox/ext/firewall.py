df53c24d1cbd276901b8078eaac6a902f5772e22
Nicoezg <ngruner@fi.uba.ar>
2025-06-22 17:33:27 -0300

    chore: removed unused code

--- pox/ext/firewall.py
+++ pox/ext/firewall.py
@@ -69,88 +69,8 @@ class Firewall (object):
         msg.match = match
         msg.priority = 49152
         self.connection.send(msg)
-        
 
-
-    """     
-    def _handle_PacketIn(self, event):
-        in_port = event.port
-        dpid = event.connection.dpid # Unique ID for the switch.
-        packet = event.parsed
-
-        if not packet.parsed:
-            log.warning("Ignoring incomplete packet")
-            return
-
-        fields = self.extract_fields(packet)
-
-        rules = self.rules_by_dpid.get(dpid)
-        if rules:
-            #log.debug("%s rules for dpid %s", str(len(rules)), str(dpid))
-            for rule in rules:
-                if self.check_matches(rule, fields):
-                    log.debug("Packet matched rule: %s", rule)
-                    if rule["action"] == "deny":
-                        self.drop_packet(event.ofp, in_port)
-                        self.install_flow(rule)
-                        return
-
-
-        # Default: allow/flood
-        #log.debug("Packet allowed through firewall")
-        msg = of.ofp_packet_out()
-        msg.data = event.ofp
-        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-        msg.in_port = in_port
-        self.connection.send(msg)
-
-
-    def extract_fields(self, packet):
-        # Basic fields
-        fields = {
-            "eth_type": packet.type,
-            "nw_proto": None, # Transport protocol
-            "nw_src": None, # IP src
-            "nw_dst": None, # IP dst
-            "tp_src": None, # src port
-            "tp_dst": None, # dst port
-        }
-
-        if fields["eth_type"] == ethernet.IP_TYPE:
-            ip_pkt = packet.payload
-            fields["nw_proto"] = ip_pkt.protocol
-            fields["nw_src"] = ip_pkt.srcip
-            fields["nw_dst"] = ip_pkt.dstip
-
-            if fields["nw_proto"] == ipv4.TCP_PROTOCOL\
-            or fields["nw_proto"] == ipv4.UDP_PROTOCOL:
-                transport_pkt = ip_pkt.payload
-                fields["tp_src"] = transport_pkt.srcport
-                fields["tp_dst"] = transport_pkt.dstport
-
-        return fields
-
-
-    def check_matches(self, rule, fields):
-        for field, val in rule.items():
-            if field == "action": # Don't compare action
-                continue
-            if val == "any":
-                continue
-            if val != fields.get(field):
-                return False
-        return True
-
-
-    def drop_packet(self, data, port):
-        msg = of.ofp_packet_out()
-        msg.data = data
-        msg.in_port = port
-        # No actions = drop
-        self.connection.send(msg) 
-    """
-
-def launch ():
+def launch():
     '''
     Starting the Firewall module
     '''

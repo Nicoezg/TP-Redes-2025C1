5dd7d96cd7ed3f22c18a01ccc009078551f19b2a
Emanuel Condo <emanuelcondo@Emanuels-MacBook-Air-2.local>
2025-06-22 01:53:57 -0300

    feat: cargar archivo config desde launch y agrego para reglas

--- pox/ext/firewall.py
+++ pox/ext/firewall.py
@@ -10,18 +10,20 @@ Teaching Assistant: Arpit Gupta
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
-from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
-from collections import namedtuple
-import os
 from pox.lib.addresses import IPAddr
 from pox.lib.packet.ethernet import ethernet
 from pox.lib.packet.ipv4 import ipv4
+from rule import Rule
 import json
 
 
 log = core.getLogger()
 policyFile = "ext/firewall-policies.json"
+protocol_dict = {
+    'tcp': ipv4.TCP_PROTOCOL,
+    'udp': ipv4.UDP_PROTOCOL,
+    'icmp': ipv4.ICMP_PROTOCOL
+}
 
 class Firewall (object):
     """
@@ -29,64 +31,37 @@ class Firewall (object):
     An Event object for that connection is passed to the __init__ function.
     """
 
-    def __init__ (self, event):
-        log.debug("Opening policy file with rules to install: %s", policyFile)
-        with open(policyFile) as f:
-            data = json.load(f)
-
-            switch_id = data["switch_id"]
-            rules = []
-
-            for rule in data["rules"]:
-                new_rule = dict(rule)
-                if "nw_src" in new_rule:
-                    new_rule["nw_src"] = IPAddr(new_rule["nw_src"])
-                rules.append(new_rule)
-
-            self.rules_by_dpid = {
-                switch_id: rules
-            }
-        log.debug("Starting firewall instance for connection %s (switch ID: %s)",\
-                event.connection, \
-                dpidToStr(event.dpid))
-    
-
+    def __init__ (self, event, rules):
         # Keep track of connection to switch.
         self.connection = event.connection
 
         # Bind PacketIn event listener.
         event.connection.addListeners(self)
-    
-        # Pre-install all rules on switch.
-        dpid = event.connection.dpid
-        rules = self.rules_by_dpid.get(dpid)
-        if rules:
-            for rule in rules:
-                self.install_flow(rule)
-            log.debug("Installed rules on switch %s", str(dpid))
+
+        for rule in rules:
+            self.install_flow(rule)
+        log.debug("Installed rules on switch %s", str(event.connection.dpid))
 
     def install_flow(self, rule):
-        if rule.get("action") != "deny":
-            log.warning("Non-deny actions not implemented")
-            return
+        log.info("Installing packet dropping rules: %s", rule)
 
         msg = of.ofp_flow_mod()
         match = of.ofp_match()
         is_ip = False # Flag if rule is for IP packet
 
-        if "nw_src" in rule:
-            match.nw_src = rule["nw_src"]
+        if rule.ip_src is not None:
+            match.nw_src = IPAddr(rule.ip_src)
             is_ip = True
-        if "nw_dst" in rule:
-            match.nw_dst = rule["nw_dst"]
+        if rule.ip_dst is not None:
+            match.nw_dst = IPAddr(rule.ip_dst)
             is_ip = True
-        if "nw_proto" in rule:
-            match.nw_proto = rule["nw_proto"]
+        if rule.protocol is not None:
+            match.nw_proto = protocol_dict[rule.protocol]
             is_ip = True
-            if "tp_src" in rule:
-                match.tp_src = rule["tp_src"]
-            if "tp_dst" in rule:
-                match.tp_dst = rule["tp_dst"]
+            if rule.port_src is not None:
+                match.tp_src = rule.port_src
+            if rule.port_dst is not None:
+                match.tp_dst = rule.port_dst
 
         if is_ip:
             match.dl_type = ethernet.IP_TYPE
@@ -179,5 +154,24 @@ def launch ():
     '''
     Starting the Firewall module
     '''
+    log.debug("Opening policy file with rules to install: %s", policyFile)
+    with open(policyFile) as f:
+        data = json.load(f)
+
+        switch_id = data["switch_id"]
+        rules = []
+
+        for rule in data["rules"]:
+            new_rule = dict(rule)
+            rules.append(Rule(new_rule))
+
+    def on_switch_up(event):
+        dpid = event.connection.dpid
+        if dpid == switch_id:
+            log.info("Switch %d connected (firewall)", dpid)
+            Firewall(event, rules)
+        else:
+            log.info("Switch %d connected", dpid)
+
     # Automagically calls Firewall(event).
-    core.openflow.addListenerByName("ConnectionUp", Firewall)
+    core.openflow.addListenerByName("ConnectionUp", on_switch_up)
